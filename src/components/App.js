91b1f9777491da1c7991b424232aeba6cf9dead4
Prince V <princev2487@gmail.com>
2022-06-26 22:28:41 +0530

    Cleaned app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,9 @@
 import React from 'react'
-// import React, {useEffect, useState} from 'react'
 
 import Sudoku from 'components/Sudoku'
 import NumberBoard from 'components/NumberBoard'
 
-
 export default function App() {
-	// const [isWideScreen, setIsWideScreen] = useState(null)
-
-	// useEffect(() => {
-	// 	const resizeListener = () => {
-	// 		setIsWideScreen(window.screen.width > 480)
-	// 	}
-	// 	resizeListener()
-	// 	window.addEventListener('resize', resizeListener)
-
-	// 	return () => window.removeEventListener('resize', resizeListener)
-	// })
-
 	return (
 		<div className='app'>
 			<main>
