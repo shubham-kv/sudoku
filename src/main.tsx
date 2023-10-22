882a5dcf4baae5b7e9202aa44640ddad24b98110
Prince V <princev2487@gmail.com>
2023-10-22 15:55:44 +0530

    chore: migrate to vite

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {App} from './App.tsx'
+
+import './styles/default.scss'
+import './styles/index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
